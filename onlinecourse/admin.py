edfb0b13102c9c0ee7605de853ed38f34a5502d9
sarqq <sara.baba@hotmail.com>
2026-02-02 03:58:20 -0500

    Register Question, Choice and Submission models

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,47 @@
+"""
+Django app development w/SQL and databases - Final Project
+
+Task 2: Import models and register model changes
+"""
+
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+# Task 2: QuestionInline class
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
 
+# Task 2: ChoiceInline class
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
 
-# Register your models here.
+# Task 2: QuestionAdmin class
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ["content"]
+
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
-
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
-
-# <HINT> Register Question and Choice models here
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+# Task 2: register Question, Choice and Submission models
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
