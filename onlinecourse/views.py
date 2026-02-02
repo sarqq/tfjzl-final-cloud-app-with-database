8dcecf6e94bcf8d75fb27e944b61626f66e798f6
sarqq <baba.sara@hotmail.com>
2026-02-02 05:04:19 -0500

    Create exam submission and evaluation views and routing

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,13 +1,19 @@
+"""
+Django app development w/SQL and databases - Final Project
+
+Task 5: Views for exam submission and evaluation
+"""
+
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-# <HINT> Import any new Models here
-from .models import Course, Enrollment
+from .models import Course, Enrollment, Question, Choice, Submission
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
 from django.contrib.auth import login, logout, authenticate
 import logging
+
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 # Create your views here.
@@ -24,11 +30,13 @@ def registration_request(request):
         first_name = request.POST['firstname']
         last_name = request.POST['lastname']
         user_exist = False
+        
         try:
             User.objects.get(username=username)
             user_exist = True
         except:
             logger.error("New user")
+        
         if not user_exist:
             user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
                                             password=password)
@@ -41,10 +49,12 @@ def registration_request(request):
 
 def login_request(request):
     context = {}
+    
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['psw']
         user = authenticate(username=username, password=password)
+        
         if user is not None:
             login(request, user)
             return redirect('onlinecourse:index')
@@ -62,11 +72,14 @@ def logout_request(request):
 
 def check_if_enrolled(user, course):
     is_enrolled = False
+    
     if user.id is not None:
         # Check if user enrolled
         num_results = Enrollment.objects.filter(user=user, course=course).count()
+        
         if num_results > 0:
             is_enrolled = True
+    
     return is_enrolled
 
 
@@ -78,9 +91,11 @@ class CourseListView(generic.ListView):
     def get_queryset(self):
         user = self.request.user
         courses = Course.objects.order_by('-total_enrollment')[:10]
+        
         for course in courses:
             if user.is_authenticated:
                 course.is_enrolled = check_if_enrolled(user, course)
+        
         return courses
 
 
@@ -102,35 +117,52 @@ def enroll(request, course_id):
 
     return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id,)))
 
-
-# <HINT> Create a submit view to create an exam submission record for a course enrollment,
-# you may implement it based on following logic:
-         # Get user and course object, then get the associated enrollment object created when the user enrolled the course
-         # Create a submission object referring to the enrollment
-         # Collect the selected choices from exam form
-         # Add each selected choice object to the submission object
-         # Redirect to show_exam_result with the submission id
-#def submit(request, course_id):
-
-
 # An example method to collect the selected choices from the exam form from the request object
 def extract_answers(request):
-   submitted_anwsers = []
-   for key in request.POST:
+    submitted_anwsers = []
+    
+    for key in request.POST:
+       
        if key.startswith('choice'):
            value = request.POST[key]
            choice_id = int(value)
            submitted_anwsers.append(choice_id)
-   return submitted_anwsers
+    
+    return submitted_anwsers
 
+# Task 5: View for exam submission
+def submit(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    enrollment = Enrollment.objects.get(user=request.user, course=course)
+    
+    submission = Submission.objects.create(enrollment=enrollment)
+    choices = submission.choices.set(extract_answers(request))
+    sub_id = submission.id
 
-# <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
-# you may implement it based on the following logic:
-        # Get course and submission based on their ids
-        # Get the selected choice ids from the submission record
-        # For each selected choice, check if it is a correct answer or not
-        # Calculate the total score
-#def show_exam_result(request, course_id, submission_id):
+    return HttpResponseRedirect(reverse(viewname="onlinecourse:exam_result", args(course_id, sub_id,)))
 
+# Task 5: View for exam result evaluation
+def show_exam_result(request, course_id, submission_id):
+    context = {}
+
+    course = get_object_or_404(Course, pk=course_id)
+    submission = Submission.objects.get(id=submission_id)
+    choices = submission.choices.all()
+
+    total_score = 0
+    questions = course.question_set.all()
+
+    for q in questions:
+        correct = question.choice_set.filter(is_correct=True)
+        selected = choices.filter(question=q)
+
+        if set(correct) == set(selected):
+            total_score += question.grade
+    
+    context["course"] = course
+    context["grade"] = total_score
+    context["choices"] = choices
+
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
 
 
